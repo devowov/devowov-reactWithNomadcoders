fbcac3fe08c8112eaecc3596dce4b0d4c44b738f
EUNJU HWANG <dev.owov@gmail.com>
2022-12-26 13:38:20 +0900

    finish - publishing - add CSS

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail(){
+    const [loading,setLoading] = useState(true);
     const {id} = useParams();
     const [movie, setMovie] = useState([]);
     const getMovie = async () => {
@@ -9,27 +11,43 @@ function Detail(){
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
             ).json();
         setMovie(json.data.movie);
+        setLoading(false);
     };
     
     useEffect(()=>{
         getMovie();
     },[])
-    return (
-    <div>
-        <h1>Detail</h1>
-        <h2>{movie.title_long}</h2>
-        <img src={movie.medium_cover_image} alt={movie.title} />
-        <div>
-            <div style={{float:'left', width:'50%'}}>
-                <h3>Year</h3>
-                <p>{movie.year}</p>
+    return (<div className={styles.loader}>
+        {loading ? (
+            
+              <span>Loading...</span>
+            
+          ) :(
+            <div>
+            <h1>Detail</h1>
+            <h2>{movie.title_long}</h2>
+            <img src={movie.medium_cover_image} alt={movie.title} className={styles.movie__img}/>
+            <div className={styles.movie__info}>
+                <div>
+                    <h3>Year</h3>
+                    <p>{movie.year}</p>
+                </div>
+                <div>
+                    <h3>Rating</h3>
+                    <p>{movie.rating}</p>
+                </div>
+                <div>
+                    <h3>Download Count</h3>
+                    <p>{movie.download_count}</p>
+                </div>
             </div>
-            <div style={{float:'left', width:'50%'}}>
-                <h3>rating</h3>
-                <p>{movie.rating}</p>
+            <div className={styles.movie__desc}>
+                <h3>Description Intro</h3>
+                <p>{movie.description_intro}</p>
             </div>
+            
+        </div>)}
         </div>
-    </div>
     );
 }
 
